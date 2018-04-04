a56f2fb09ad6b049e24e37cbad3ca21be3c19de0
TheCrimulo <thecrimulo@gmail.com>
2018-04-04 19:28:25 +0200

    Added an example to be run

---
+++ examples/link.lua
@@ -0,0 +1,20 @@
+local Kristal = require "kristal"
+
+-- Accounts
+local khugepoopy = Kristal.Account:new {address="khugepoopy"}
+local dv_kst     = Kristal.Account:new {name="dv.kst", own=true, key="pkey", format="dvseal"}
+
+-- Agent
+local agent = Kristal.Transaction.Agent:new ()
+
+-- Handler
+local handler = Kristal.Transaction.Handler:new ()
+handler:on ("donate-to-lem@dv.kst", function(Transaction,Metadata)
+  agent:make (dv_kst, khugepoopy, Transaction.value, {donate="true",message="Someone donated through dv.kst"})
+end)
+
+-- Start listening to the Websocket
+agent:socketConnect (dv_kst, agent:wrap, handler)
+
+-- Tell Kristal to start executing
+Kristal.go ()
